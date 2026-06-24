fe9be1e431b9587fa313655124bb66d4affb7221
TenTen Liang <nemo9527@hotmail.com>
2026-06-24 18:16:02 +0800

    适配不同电脑的数据路径

--- config.py
+++ config.py
@@ -1,25 +1,63 @@
 from pathlib import Path
 
-TRADE_DIR = Path(
-    r"D:\code\PycharmProjects\red\backtest"
+# ==========================
+# 自动识别盘符
+# ==========================
+
+if Path(
+    r"D:\code\PycharmProjects\red"
+).exists():
+
+    ROOT_DIR = Path(
+        r"D:\code\PycharmProjects\red"
+    )
+
+    DATA_ROOT = Path(
+        r"D:\qdata"
+    )
+
+else:
+
+    ROOT_DIR = Path(
+        r"C:\code\red"
+    )
+
+    DATA_ROOT = Path(
+        r"C:\code\data"
+    )
+
+# ==========================
+# 目录
+# ==========================
+
+TRADE_DIR = (
+    ROOT_DIR
+    / "backtest"
 )
 
-DATA_DIR = Path(
-    r"D:\qdata\stock-5m-close-price"
+RESULT_DIR = (
+    ROOT_DIR
+    / "result"
 )
 
-RESULT_DIR = Path(
-    r"D:\code\PycharmProjects\red\result"
+DATA_DIR = (
+    DATA_ROOT
+    / "stock-5m-close-price"
 )
 
-TRADING_DATA_DIR = Path(
-    r"D:\qdata\stock-trading-data"
+TRADING_DATA_DIR = (
+    DATA_ROOT
+    / "stock-trading-data"
 )
 
 RESULT_DIR.mkdir(
     parents=True,
     exist_ok=True
 )
+
+# ==========================
+# 回测参数
+# ==========================
 
 INIT_CASH = 500000
 
@@ -31,26 +69,29 @@ COMPARE_TIME = "1455"
 
 LIMIT_RATIO = 0.998
 
-# 是否启用趋势股判断
-# True:
-#   趋势股 -> 原策略
-#   强势股 -> 5%止盈
-#
-# False:
-#   所有股票都按趋势策略
-ENABLE_TREND_FILTER = True
+# ==========================
+# 趋势/强势开关
+# ==========================
 
-# 是否参与强势股
+ENABLE_TREND_FILTER = True
 
 ENABLE_STRONG_STOCK = True
 
+# ==========================
 # 趋势股判定
+# ==========================
+
 BREAKOUT_PCT = 7
 
-# 回撤卖出比例
+# ==========================
+# 固定回撤
+# ==========================
+
 DRAWDOWN_RATIO = 0.05
 
-# 趋势股动态回撤
+# ==========================
+# 动态回撤
+# ==========================
 
 # 盈利20%以内
 DRAWDOWN_RATIO_LOW = 0.10
@@ -60,14 +101,17 @@ DRAWDOWN_RATIO_MID = 0.08
 
 # 盈利50%以上
 DRAWDOWN_RATIO_HIGH = 0.05
-# 强势股止盈比例
-# 0.05 = 5%
-# 0.08 = 8%
-# 0.10 = 10%
+
+# ==========================
+# 强势股止盈
+# ==========================
+
 STRONG_TAKE_PROFIT = 0.06
 
-# 趋势股最大持仓天数
+# ==========================
+# 最大持仓天数
+# ==========================
+
 TREND_MAX_HOLD_DAYS = 0
 
-# 强势股最大持仓天数
 STRONG_MAX_HOLD_DAYS = 0
